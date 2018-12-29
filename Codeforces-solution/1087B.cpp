6c0dfcd764011a1fe8dbad798e377447bde8b471
pranto saha <sahapranto32@gmail.com>
2018-12-29 17:58:25 +0600

    Div Times Mod

---
+++ Codeforces-solution/1087B.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+#define MAX 10000005
+#define N 100005
+
+#define max2(a,b) ((a>b)?a:b)
+#define max3(a,b,c) max2(max2(a,b),c)
+#define min2(a,b) ((a<b)?a:b)
+#define min3(a,b,c) (min2(a,b),c)
+#define gcd(a,b) __gcd(a,b)
+#define pi 2.0*acos(0.0)
+
+#define sci(a) scanf("%d", &a)
+#define sci2(a,b) scanf("%d%d", &a, &b)
+#define sci3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scd(a) scanf("%lf", &a)
+#define scd2(a,b) scanf("%lf%lf",&a,&b)
+#define scd3(a, b, c) scanf("%lf%lf%lf", &a, &b, &c)
+
+
+//loop
+#define rep(ii,aa,bb) for(ll (ii)=ll(aa);(ii)<(ll)(bb);++(ii))
+#define per(ii,aa,bb) for(ll (ii)=ll(aa)-1;(ii)>=(ll)(bb);--(ii))
+
+#define file_in(); freopen("input.txt", "r", stdin);
+#define file_out(); freopen("output.txt", "w", stdout);
+#define _fastIO() ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+typedef long long ll;
+int main()
+{
+    ll n, k, x;
+    cin >> n >> k;
+    vector<ll> vec;
+    for(ll rem = 1; rem<k; rem++){
+        if(n % rem == 0){
+            ll r =n/rem;
+            x = k *r;
+            x = x + rem;
+            ll test = (x/k) * (x%k);
+            if(test == n)
+                vec.push_back(x);
+        }
+    }
+    sort(vec.begin(), vec.end());
+    cout << vec[0] << endl;
+    return 0;
+}
+
+
