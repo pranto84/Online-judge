a4add3030eb08dcb539b4b1a8361fc79b3ef97b8
pranto saha <sahapranto32@gmail.com>
2018-12-29 17:43:12 +0600

    Find Divisible

---
+++ Codeforces-solution/1096A.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define MAX 10000005
+#define N 100005
+
+#define max2(a,b) ((a>b)?a:b)
+#define max3(a,b,c) max2(max2(a,b),c)
+#define min2(a,b) ((a<b)?a:b)
+#define min3(a,b,c) (min2(a,b),c)
+#define gcd(a,b) __gcd(a,b)
+#define pi 2.0*acos(0.0)
+#define     pb  push_back
+#define     mp  make_pair
+
+
+#define sci(a) scanf("%d", &a)
+#define sci2(a,b) scanf("%d%d", &a, &b)
+#define sci3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scd(a) scanf("%lf", &a)
+#define scd2(a,b) scanf("%lf%lf",&a,&b)
+#define scd3(a, b, c) scanf("%lf%lf%lf", &a, &b, &c)
+
+
+//loop
+#define rep(ii,aa,bb) for(ll (ii)=ll(aa);(ii)<(ll)(bb);++(ii))
+#define per(ii,aa,bb) for(ll (ii)=ll(aa)-1;(ii)>=(ll)(bb);--(ii))
+
+#define file_in(); freopen("input.txt", "r", stdin);
+#define file_out(); freopen("output.txt", "w", stdout);
+#define _fastIO() ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+typedef long long ll;
+int main()
+{
+    int csno=1,T;
+    sci(T);
+    while(T--){
+        int l, r;
+        sci2(l,r);
+        int x, y;
+        x =min(l,r);
+        y = x*2;
+        cout << x << " "<<y << endl;
+    }
+
+    return 0;
+}
+
+
