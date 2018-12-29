c075977796dfd41bc355ee39ebf72c0d84eebc67
pranto saha <sahapranto32@gmail.com>
2018-12-29 17:49:28 +0600

    Minimum Diameter Tree

---
+++ Codeforces-solution/1087D.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+#define MAX 10000005
+#define N 100005
+
+#define max2(a,b) ((a>b)?a:b)
+#define max3(a,b,c) max2(max2(a,b),c)
+#define min2(a,b) ((a<b)?a:b)
+#define min3(a,b,c) (min2(a,b),c)
+#define gcd(a,b) __gcd(a,b)
+#define pi 2.0*acos(0.0)
+
+#define sci(a) scanf("%d", &a)
+#define sci2(a,b) scanf("%d%d", &a, &b)
+#define sci3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scd(a) scanf("%lf", &a)
+#define scd2(a,b) scanf("%lf%lf",&a,&b)
+#define scd3(a, b, c) scanf("%lf%lf%lf", &a, &b, &c)
+
+
+//loop
+#define rep(ii,aa,bb) for(ll (ii)=ll(aa);(ii)<(ll)(bb);++(ii))
+#define per(ii,aa,bb) for(ll (ii)=ll(aa)-1;(ii)>=(ll)(bb);--(ii))
+
+#define file_in(); freopen("input.txt", "r", stdin);
+#define file_out(); freopen("output.txt", "w", stdout);
+#define _fastIO() ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+typedef long long ll;
+int main()
+{
+    int n, s;
+    while(sci2(n,s) == 2){
+        int ara[n+5];
+        memset(ara,0,sizeof(ara));
+        int a,b;
+        for(int i=1; i<n; i++){
+            sci2(a,b);
+            ara[a]++;
+            ara[b]++;
+        }
+        int cnt = 0;
+        for(int i=1; i<=n; i++)
+            if(ara[i]==1)
+                cnt++;
+        double ans;
+        ans = (double)s / (double)cnt;
+        ans = ans * 2.0;
+        printf("%.18lf\n",ans);
+    }
+    return 0;
+}
