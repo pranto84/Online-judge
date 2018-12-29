7c409be7c00697fe92e94d003c53ccf8b9969186
pranto saha <sahapranto32@gmail.com>
2018-12-29 18:01:39 +0600

    Great Vova Wall (Version 2)

---
+++ Codeforces-solution/1092D2.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+#define MAX 10000005
+#define N 100005
+
+#define max2(a,b) ((a>b)?a:b)
+#define max3(a,b,c) max2(max2(a,b),c)
+#define min2(a,b) ((a<b)?a:b)
+#define min3(a,b,c) (min2(a,b),c)
+#define gcd(a,b) __gcd(a,b)
+#define pi 2.0*acos(0.0)
+
+#define sci(a) scanf("%d", &a)
+#define sci2(a,b) scanf("%d%d", &a, &b)
+#define sci3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scd(a) scanf("%lf", &a)
+#define scd2(a,b) scanf("%lf%lf",&a,&b)
+#define scd3(a, b, c) scanf("%lf%lf%lf", &a, &b, &c)
+
+
+//loop
+#define rep(ii,aa,bb) for(ll (ii)=ll(aa);(ii)<(ll)(bb);++(ii))
+#define per(ii,aa,bb) for(ll (ii)=ll(aa)-1;(ii)>=(ll)(bb);--(ii))
+
+#define file_in(); freopen("input.txt", "r", stdin);
+#define file_out(); freopen("output.txt", "w", stdout);
+#define _fastIO() ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+typedef long long ll;
+int main()
+{
+    int n;
+    sci(n);
+    int ara[n+5];
+    stack<int>st;
+    bool temp = true;
+    int current_max = 0;
+    for(int i=0; i<n; i++){
+        sci(ara[i]);
+        if(!st.empty()){
+            if(st.top() == ara[i])
+                st.pop();
+            else if(st.top() < ara[i]){
+                temp = false;
+                break;
+            }
+            else
+                st.push(ara[i]);
+        }
+        else
+            st.push(ara[i]);
+        current_max =  max(current_max, ara[i]);
+    }
+    if(st.empty() && temp)
+        cout << "YES\n";
+    else if(st.size() == 1 && temp){
+        if(st.top() >= current_max)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    else
+        cout << "NO\n";
+    return 0;
+}
+
