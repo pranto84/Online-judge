4e3e66a3dd606b1446bd3480328e1c4667be94db
pranto saha <sahapranto32@gmail.com>
2018-12-29 18:14:46 +0600

    Uniform String

---
+++ Codeforces-solution/1092A.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+#define MAX 10000005
+#define N 100005
+
+#define max2(a,b) ((a>b)?a:b)
+#define max3(a,b,c) max2(max2(a,b),c)
+#define min2(a,b) ((a<b)?a:b)
+#define min3(a,b,c) (min2(a,b),c)
+#define gcd(a,b) __gcd(a,b)
+#define pi 2.0*acos(0.0)
+
+#define sci(a) scanf("%d", &a)
+#define sci2(a,b) scanf("%d%d", &a, &b)
+#define sci3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scd(a) scanf("%lf", &a)
+#define scd2(a,b) scanf("%lf%lf",&a,&b)
+#define scd3(a, b, c) scanf("%lf%lf%lf", &a, &b, &c)
+
+
+//loop
+#define rep(ii,aa,bb) for(ll (ii)=ll(aa);(ii)<(ll)(bb);++(ii))
+#define per(ii,aa,bb) for(ll (ii)=ll(aa)-1;(ii)>=(ll)(bb);--(ii))
+
+#define file_in(); freopen("input.txt", "r", stdin);
+#define file_out(); freopen("output.txt", "w", stdout);
+#define _fastIO() ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+typedef long long ll;
+int main()
+{
+    int t, n, k;
+    sci(t);
+    while(t--){
+        sci2(n,k);
+        int fre = n/k;
+        char ch = 'a'-1;
+        for(int i=1; i<=k; i++){
+            ch++;
+            for(int i=1; i<=fre; i++)
+                cout << ch;
+        }
+        int rest  = n%k;
+        for(int i=1; i<=rest; i++)
+            cout << ch;
+        cout << endl;
+    }
+    return 0;
+}
